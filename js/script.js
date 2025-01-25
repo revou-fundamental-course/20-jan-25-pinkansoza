c527905a962e6c3d02b953372192f451d5c1974c
Pinkan Sofia Zahra <zahrapinkan19@gmail.com>
2025-01-25 18:04:54 +0700

    new update 5

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // Ini File Javascript
+
+// Menambahkan event listener pada tombol dengan ID "calculate"
 document.getElementById("calculate").addEventListener("click", function () {
     const weight = parseFloat(document.getElementById("weight").value);
     const height = parseFloat(document.getElementById("height").value) / 100;
     const gender = document.querySelector('input[name="gender"]:checked').value;
 
+// Validasi Input
     if (!weight || !height) {
         alert("Mohon isi berat dan tinggi badan dengan benar.");
         return;
     }
 
+// Menghitung nilai BMI
     const bmi = (weight / (height * height)).toFixed(1);
     const result = document.getElementById("result");
     const bmiGender = document.getElementById("bmiGender");
@@ -17,11 +21,13 @@ document.getElementById("calculate").addEventListener("click", function () {
     const bmiValueBottom = document.getElementById("bmiValueBottom")
     const bmiAdvice = document.getElementById("bmiAdvice");
     const bmiNote = document.getElementById("bmiNote");
-    
+ 
+// Menampilkan informasi BMI
     bmiGender.textContent = `BMI Untuk ${gender}`;
     bmiValue.textContent = `${bmi}`;
     bmiNote.textContent = "BMI tidak sepenuhnya mewakili diagnosis menyeluruh dari kesehatan tubuh dan resiko penyakit seseorang. Anda perlu konsultasi lebih lanjut mengenai resiko dan kekhawatiran Anda terkait dengan berat badan Anda."
 
+// Menentukan kategori BMI
     if (bmi < 18.5) {
         bmiStatus.textContent = "Berat badan kurang";
         bmiValueBottom.textContent = "Hasil BMI kurang dari 18.5";
@@ -29,7 +35,7 @@ document.getElementById("calculate").addEventListener("click", function () {
     } else if (bmi >= 18.5 && bmi <= 24.9) {
         bmiStatus.textContent = "Berat badan ideal";
         bmiValueBottom.textContent = "Hasil BMI diantara 18.5 - 24.9";
-        bmiAdvice.textContent = "Selamat! Anda memiliki berat badan yang ideal.";
+        bmiAdvice.textContent = "Selamat! Anda memiliki berat badan yang ideal! Tetap pertahankan gaya hidup sehat ini, karena kesehatan adalah investasi terbaik untuk masa depan. Terus konsisten, nikmati prosesnya, dan tetap percaya diri!";
     } else if (bmi >= 25 && bmi <= 29.9) {
         bmiStatus.textContent = "Berat badan lebih";
         bmiValueBottom.textContent = "Hasil BMI diantara 25 - 29.9";
@@ -40,6 +46,7 @@ document.getElementById("calculate").addEventListener("click", function () {
         bmiAdvice.textContent = "Anda berada dalam kategori obesitas. Disarankan untuk berkonsultasi dengan dokter untuk penanganan lebih lanjut.";
     }
 
+// Menampilkan button Konsultasi dan Registrasi
     document.querySelectorAll('.button2').forEach(button => {
         button.classList.add('visible');
     });
