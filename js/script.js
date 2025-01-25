a479b8b8ba22420cebffee165c8670c719116f00
Pinkan Sofia Zahra <zahrapinkan19@gmail.com>
2025-01-25 15:34:17 +0700

    new update 4

--- js/script.js
+++ js/script.js
@@ -12,27 +12,37 @@ document.getElementById("calculate").addEventListener("click", function () {
     const bmi = (weight / (height * height)).toFixed(1);
     const result = document.getElementById("result");
     const bmiGender = document.getElementById("bmiGender");
-    const bmiValue = document.getElementById("bmiValue");
     const bmiStatus = document.getElementById("bmiStatus");
+    const bmiValue = document.getElementById("bmiValue");
+    const bmiValueBottom = document.getElementById("bmiValueBottom")
     const bmiAdvice = document.getElementById("bmiAdvice");
-
+    const bmiNote = document.getElementById("bmiNote");
+    
     bmiGender.textContent = `BMI Untuk ${gender}`;
-    bmiValue.textContent = `BMI Kamu: ${bmi}`;
+    bmiValue.textContent = `${bmi}`;
+    bmiNote.textContent = "BMI tidak sepenuhnya mewakili diagnosis menyeluruh dari kesehatan tubuh dan resiko penyakit seseorang. Anda perlu konsultasi lebih lanjut mengenai resiko dan kekhawatiran Anda terkait dengan berat badan Anda."
 
     if (bmi < 18.5) {
         bmiStatus.textContent = "Berat badan kurang";
+        bmiValueBottom.textContent = "Hasil BMI kurang dari 18.5";
         bmiAdvice.textContent = "Anda berada dalam kategori berat badan kurang. Disarankan untuk meningkatkan asupan kalori dan berkonsultasi dengan ahli gizi.";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
         bmiStatus.textContent = "Berat badan ideal";
+        bmiValueBottom.textContent = "Hasil BMI diantara 18.5 - 24.9";
         bmiAdvice.textContent = "Selamat! Anda memiliki berat badan yang ideal.";
     } else if (bmi >= 25 && bmi <= 29.9) {
         bmiStatus.textContent = "Berat badan lebih";
+        bmiValueBottom.textContent = "Hasil BMI diantara 25 - 29.9";
         bmiAdvice.textContent = "Anda berada dalam kategori overweight. Cobalah untuk menjaga pola makan sehat dan rutin berolahraga.";
     } else {
         bmiStatus.textContent = "Obesitas";
+        bmiValueBottom.textContent = "Hasil BMI lebih dari 30.0";
         bmiAdvice.textContent = "Anda berada dalam kategori obesitas. Disarankan untuk berkonsultasi dengan dokter untuk penanganan lebih lanjut.";
     }
 
+    document.querySelectorAll('.button2').forEach(button => {
+        button.classList.add('visible');
+    });
+
     result.style.display = "block";
 });
-
